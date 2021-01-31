4e882076389cf134d6497f74179233f16c9eabd5
LakshanViranga <lakshanviranga0000@gmail.com>
2021-01-31 22:55:42 +0530

    database changed

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
-$user = 'epicxcom@localhost';
-$pas = '';
+$user = 'epicxcom_leeta';
+$pas = '123Priyanga*';
 $database = 'epicxcom_id15860293_datadb';
 
 $konek = mysqli_connect($host, $user, $pas, $database);
