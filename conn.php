05d9d4f77efd1f6fbc4a26c917a7867e74b1517b
LakshanViranga <lakshanviranga0000@gmail.com>
2021-01-31 22:49:35 +0530

    database created

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$user = 'id15860293_datauser';
-$pas = 'OksO@F4tPuTFUZ-[';
-$database = 'id15860293_datadb';
+$user = 'epicxcom@localhost';
+$pas = '';
+$database = 'epicxcom_id15860293_datadb';
 
 $konek = mysqli_connect($host, $user, $pas, $database);
 
